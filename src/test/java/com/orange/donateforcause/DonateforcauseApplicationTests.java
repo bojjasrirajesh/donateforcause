1659b6e1f88ea4bcf045790713c91e82c58e4a76
Rajesh Bojja <bojja.r@HCL.COM>
2020-02-14 15:10:58 +0530

    test cases for user and donors apis

--- src/test/java/com/orange/donateforcause/DonateforcauseApplicationTests.java
+++ src/test/java/com/orange/donateforcause/DonateforcauseApplicationTests.java
@@ -1,13 +1,7 @@
 package com.orange.donateforcause;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DonateforcauseApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
